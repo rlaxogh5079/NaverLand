5e2f5ae34294f02e50ad1c91f36d800bdf788075
Rainbow5079 <rlaxogh507906@gmail.com>
2022-02-24 15:31:39 +0900

    update scrapy.sh

--- scrapy.sh
+++ scrapy.sh
@@ -1,5 +1,9 @@
 cd ~/NaverLand
 
+CORTARNO=$(<~/NaverLand/naver_land_crawler/spiders/CORTARNO.txt)
+
+docker run -d --name naver_land_crawler$CORTARNO --link mysqlserver naver_land_crawler
+
 scrapy crawl RegionSpider
 
 scrapy crawl NaverLandSpider
