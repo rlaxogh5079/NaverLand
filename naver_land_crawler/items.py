ff8834ad86b5ec7b3a25ca1c71b3d146f3859c71
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-02-27 22:58:33 +0900

    update items.py

--- naver_land_crawler/items.py
+++ naver_land_crawler/items.py
@@ -3,19 +3,17 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-from scrapy import Item,Field
+from scrapy import Item, Field
 
 
 class NaverLandCrawlerItem(Item):
-    
-    _0atclNo = Field() # 건물 고유 번호
 
-    _1company = Field() # 건물 건설사 이름
+    _0atclNo = Field()  # 건물 고유 번호
 
-    _2place = Field() # 건물 위치
-    
-    _3price = Field() # 건물 가격 (예: 전세 9억, 전세 17억 등등)
+    _1place = Field()  # 건물 위치
 
-    _4pyeong = Field() # 건물 평수
-    
-    _5fac = Field() # 건물 주변 편의시설 정보
+    _2price = Field()  # 건물 가격 (예: 전세 9억, 전세 17억 등등)
+
+    _3pyeong = Field()  # 건물 평수
+
+    _4fac = Field()  # 건물 주변 편의시설 정보
