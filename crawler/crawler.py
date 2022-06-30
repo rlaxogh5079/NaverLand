fbd8f4bb1982a3c0666c667f3c0a38c5282bce21
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-06-30 22:27:35 +0900

    :sparkles: details 로드 추가

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,10 +1,25 @@
 import requests
-from CORTARNOS import CORTARNOS
+from headers import headers
 
-CON_URL = "https://m.land.naver.com/cluster/ajax/articleList?rletTpCd=APT&tradTpCd=all&z=9&cortarNo={}&page={}"
+CON_URL = "https://new.land.naver.com/api/regions/complexes?cortarNo={}"
+DETAIL_URL = "https://new.land.naver.com/api/complexes/overview/{}?complexNo={}"
 
-def get_item():
-    for CORTARNO in CORTARNOS:
-        page = 1
-        while True:
-            request_url = CON_URL.format(CORTARNO, page)
+
+def get_complexList(CORTARNO: str) -> list:
+    request_url = CON_URL.format(CORTARNO)
+    response = requests.get(request_url, headers=headers)
+    complexList = response.json()["complexList"]
+    return complexList
+
+
+def get_details(complexList: list) -> list:
+    for complex in complexList:
+        complexNo = complex["complexNo"]
+        request_url = DETAIL_URL.format(complexNo, complexNo)
+        response = requests.get(request_url, headers=headers)
+        details = response.json()
+        yield details
+
+
+def get_items(details: dict) -> dict:
+    print(details[0])
